1daeb3fd48015b2588a4b93a7183739845f63c6e
TakuyaKikuchi <TakuyaKikuchi@TakuyaKikuchi-no-MacBook-Air.local>
2019-01-01 16:44:47 +0900

    master development for react

--- test/src/index.js
+++ test/src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//ES6の記法
+const name = {
+  firstName: 'Takuya',
+  lastName: 'Kikuchi'
+}
+let  count = 0;
+for (let i = 0; i < 10; i++) {
+  count++;
+}
+
 function Hello(params) {
   return(
     <div>
       <h1>
-        こんにちは
+        こんにちは { name.lastName + name.firstName } さん { count + 1 }回目の訪問ですね。
       </h1>
       <p>
         Helloコンポーネントが書き出されました！

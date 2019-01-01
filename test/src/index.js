f474a20e54b866e6a5feb43ab9b948dae04120bd
TakuyaKikuchi <TakuyaKikuchi@TakuyaKikuchi-no-MacBook-Air.local>
2019-01-01 16:04:45 +0900

    master development for react

--- test/src/index.js
+++ test/src/index.js
@@ -4,7 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Hello(params) {
+  return(
+    <div>
+      <h1>
+        こんにちは
+      </h1>
+      <p>
+        Helloコンポーネントが書き出されました！
+      </p>
+    </div>
+  );
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
